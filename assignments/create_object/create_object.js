58795c0927a9e66eb6847bbdf0c45829e053f147
nagendra kothapalli <nagendrakdv94@gmail.com>
2022-01-24 21:48:42 +0530

    create_obj

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    const person={}
+    for (var i=0;i<arr.length;i++){
+        if (i%2===0){
+            person[arr[i]]=arr[i+1]
+        }
+    }
+    return person
 }
 
 module.exports = CreateObject;
